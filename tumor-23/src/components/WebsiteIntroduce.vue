cb88b9d61fb9d42e605e87813f9b0bec845bf226
bluebean-cloud <1396211327@qq.com>
2024-01-17 20:43:57 +0800

    修改头图

---
+++ tumor-23/src/components/WebsiteIntroduce.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    网站介绍
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- tumor-23/src/main.ts
+++ tumor-23/src/main.ts
@@ -16,7 +16,7 @@ import "uno.css";
 
 // If you want to use ElMessage, import it.
 import "element-plus/theme-chalk/src/message.scss";
-import router from "./router.ts";
+import router from "./router";
 
 const app = createApp(App);
 // app.use(ElementPlus);
