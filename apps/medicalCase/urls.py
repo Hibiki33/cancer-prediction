659cd3f74f825c2e3140195e6c4fb59a39d19541
wnico@wsl <wnico233@outlook.com>
2024-03-15 16:59:45 +0800

    Revert "chunk uploads"
    This reverts commit 1f74d0fc94abefccbe20800a212c61f9d88cac27.

--- apps/medicalCase/urls.py
+++ apps/medicalCase/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .api.interaction import get_schedule, upload_picture
-from .views import upload_pic, upload_chunk, combine_chunks
+from .views import upload_pic
 
 urlpatterns = [
-    path('get_schedule', get_schedule, name='get_schedule'),
+    path('get_schedule/', get_schedule, name='get_schedule'),
     # path('upload_picture/', upload_picture, name='upload_picture'),
-    path('upload_pic', upload_pic, name='upload_pic'),
-    path('combine_chunks/<str:file_hash>', combine_chunks, name='combine_chunks'),
-    path('upload_chunk', upload_chunk, name='upload_chunk'),
+    path('upload_pic/', upload_pic, name='upload_pic'),
 ]
--- cancer_prediction_backend/settings.py
+++ cancer_prediction_backend/settings.py
@@ -18,6 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -46,10 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.user',
     'apps.medicalCase',
-    'apps.fileManager',
     'corsheaders',
     'rest_framework'
-] 
+]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
